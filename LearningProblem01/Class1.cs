852ac642bd7cbf805549df375248aa9173d018be
Komal <divyavairagade@gmail.com>
2022-04-14 16:29:47 +0530

    emp hrs and total wage

--- LearningProblem01/Class1.cs
+++ LearningProblem01/Class1.cs
@@ -13,13 +13,13 @@ namespace LearningProblem01
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 10;
         public const int NUM_OF_WORKING_DAYS = 20;
-
+        public const int MAX_HRS_IN_MONTH = 100;
         public static void EmployeeAttendance()
         {
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
-
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -36,10 +36,10 @@ namespace LearningProblem01
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                totalEmpWage += empWage;
-                Console.WriteLine("Day#:" + day + " Emp Wage : " + empWage);
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#:" + totalWorkingDays + " Emp Hrs : " + empHrs);
             }
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         }
     }
