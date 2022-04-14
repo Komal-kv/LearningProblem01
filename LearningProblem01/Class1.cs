eab75ac31fe87664c265c6e3ccca4a1644873e85
Komal <divyavairagade@gmail.com>
2022-04-14 16:23:44 +0530

    switch case

--- LearningProblem01/Class1.cs
+++ LearningProblem01/Class1.cs
@@ -8,31 +8,33 @@ namespace LearningProblem01
 {
     internal class Class1
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+
         public static void EmployeeAttendance()
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
 
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 8;
+                    break;
+
+                case IS_FULL_TIME:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Part Time Emp Wage :" + empWage);
+            Console.WriteLine("Part Time Emp Wage Using Switch case :" + empWage);
         }
     }
 }
