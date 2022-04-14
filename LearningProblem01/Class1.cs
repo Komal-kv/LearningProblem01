15497dbe58981b664c31daef2c5d0a16729e3e0f
Komal <divyavairagade@gmail.com>
2022-04-14 16:25:08 +0530

    total emp wage

--- LearningProblem01/Class1.cs
+++ LearningProblem01/Class1.cs
@@ -10,31 +10,36 @@ namespace LearningProblem01
     {
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
+        public const int EMP_RATE_PER_HOUR = 10;
+        public const int NUM_OF_WORKING_DAYS = 20;
 
         public static void EmployeeAttendance()
         {
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int empHrs = 0, empWage = 0, totalEmpWage = 0;
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 8;
-                    break;
 
-                case IS_FULL_TIME:
-                    empHrs = 4;
-                    break;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 8;
+                        break;
 
-                default:
-                    empHrs = 0;
-                    break;
+                    case IS_FULL_TIME:
+                        empHrs = 4;
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWage = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWage;
+                Console.WriteLine("Day#:" + day + " Emp Wage : " + empWage);
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Part Time Emp Wage Using Switch case :" + empWage);
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         }
     }
 }
